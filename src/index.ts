a9011609f24191ab5f162b6afd5080fc7b22830a
Lionel Verrier <lgv3lee@gmail.com>
2023-03-30 16:27:50 -0400

    charactersCollection & LinkedList abstract class implementation

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([100, 3, -5, 0])
-// const sorter = new Sorter(numbersCollection)
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([100, 35, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('Xaayb');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -19,6 +17,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(45);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
